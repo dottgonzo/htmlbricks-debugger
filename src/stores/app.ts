b5e712338953b80a2649145dcb3bf6492d194ee2
Dario Caruso <darioyzf@gmail.com>
2022-03-28 20:13:28 +0200

    update pkg

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -2,7 +2,7 @@ import { readable, writable, type Readable, type Writable } from 'svelte/store';
 import type allComponentsList from '@htmlbricks/hb-bundle/release/list.json';
 
 export const pageName: Writable<string> = writable('');
-export const componentsVersion: Readable<string> = readable('0.9.21');
+export const componentsVersion: Readable<string> = readable('0.10.0');
 export const debugVersion: Writable<string> = writable(null);
 // export const defaultRootRepo: Readable<string> = readable('@htmlbricks');
 // export const rootRepo: Writable<string> = writable(null);
