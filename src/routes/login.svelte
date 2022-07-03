3ef2526c6f754dd168fcae1eff66035a81a70c7d
dottgonzo <dev@dariocaruso.info>
2022-07-03 19:23:00 +0200

    wip on login

---
+++ src/routes/login.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import { addComponent, LanguageTranslator } from '@htmlbricks/hb-jsutils';
+
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import { siteUrl, oauth2providers, componentsVersion } from '../stores/app';
+	onMount(() => {
+		addComponent({ repoName: '@htmlbricks/hb-layout', version: $componentsVersion });
+		addComponent({ repoName: '@htmlbricks/hb-auth', version: $componentsVersion });
+	});
+</script>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css"
+	/>
+</svelte:head>
+<hb-layout
+	on:navbarSlotClick={(e) => {
+		goto('/');
+	}}
+	pagename="Access"
+	page_title="Access"
+	onescreen="yes"
+	companytitle="fff"
+	companylogouri=""
+	cookielaw=""
+	policies={JSON.stringify([
+		{
+			label: 'Privacy Policy',
+			key: 'privacypolicy',
+			link: `${$siteUrl}/docs/privacy`
+		},
+		{
+			label: 'Cookie Law',
+			key: 'cookielaw',
+			link: `${$siteUrl}/docs/cookielaw`
+		}
+	])}
+	company={JSON.stringify({
+		logoUri: '/monochromelogoonly_512.png',
+		siteName: 'freewebcomponents.com',
+		companyName: 'INGECO S.R.L.',
+		registration: '2022 dev',
+		description: `freewebcomponents.com - Free Web Components - HTML Bricks`,
+		vatNumber: 'aa - ffffff'
+	})}
+	sidebar={JSON.stringify({
+		logo: '/monochromelogoonly_512.png',
+		title: 'HtmlB',
+		type: 'autohide'
+	})}
+	contacts={JSON.stringify({
+		sites: [{ label: 'dariocaruso.info', uri: 'https://dariocaruso.info' }],
+		phones: [{ number: '6666666666666' }],
+		addresses: [],
+		emails: []
+	})}
+	socials={JSON.stringify({
+		github: { pageUri: 'https://github.com/dottgonzo/bootstrap-webcomponents-monorepo' }
+	})}
+>
+	<div slot="page" style="position: absolute;height: 100%;top: 0px;width: 100%;">
+		<hb-auth
+			oauth2providers={JSON.stringify($oauth2providers)}
+			type="login"
+			logouri="/banner_one_all.png"
+			on:getProviderToken={(e) => {
+				console.log('oauth', e.detail);
+			}}
+		/>
+	</div>
+</hb-layout>
+
+<style lang="scss" global>
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+	@import url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css');
+	$primary: #07689f;
+	$secondary: #c9d6df;
+	$success: #11d3bc;
+	$danger: #f67280;
+	$info: #a2d5f2;
+	$warning: #ff7e67;
+	$light: #fafafa;
+	$dark: #4e4e4e;
+	@import 'bootstrap/scss/bootstrap';
+
+	body {
+		font-family: 'Roboto', sans-serif;
+	}
+</style>
--- src/stores/app.ts
+++ src/stores/app.ts
@@ -5,10 +5,28 @@ export type allComponentsList = {
 	packages: ComponentShortSetup[];
 	version: string;
 };
+const googleClientId = '737686426982-4b7qenmb8jl7rf1j4jp649gai7t3jh00.apps.googleusercontent.com'
+const googleScope = 'https%3A//www.googleapis.com/auth/userinfo.email'
+const productionFontendUrl = 'https://demo.freewebcomponents.com'
+const developmentFontendUrl = 'http://localhost:3000'
 
+export const oauth2providers: Readable<{ provider: string, uri: string }[]> = readable([
+	{
+		provider: 'google',
+		uri: `https://accounts.google.com/o/oauth2/v2/auth?scope=${googleScope}&include_granted_scopes=true&response_type=token&state=state_parameter_passthrough_value&redirect_uri=${developmentFontendUrl}/login&client_id=${googleClientId}`
+	},
+	{
+		provider: 'facebook',
+		uri: ''
+	},
+	{
+		provider: 'twitter',
+		uri: ''
+	}
+])
 export const pageName: Writable<string> = writable('');
 export const pageTitle: Writable<string> = writable('');
-export const componentsVersion: Readable<string> = readable('0.14.0');
+export const componentsVersion: Readable<string> = readable('0.15.10');
 export const debugVersion: Writable<string> = writable(null);
 // export const defaultRootRepo: Readable<string> = readable('@htmlbricks');
 // export const rootRepo: Writable<string> = writable(null);
