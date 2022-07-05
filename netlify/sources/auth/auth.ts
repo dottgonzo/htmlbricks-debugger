e5cd69c271be484dba8c8ed272113fb1c0084bd5
dottgonzo <dev@dariocaruso.info>
2022-07-05 03:24:47 +0200

    ref

--- netlify/sources/auth/auth.ts
+++ netlify/sources/auth/auth.ts
@@ -1,6 +1,6 @@
 import serverless from 'serverless-http'
 import router from 'mongo-passport-auth-lib/serversingle'
-export const handler = serverless(router({
+const app = router({
     "mongoUri": "mongodb+srv://htmlbricks:v2HVRb4UiCgf2bre@cluster0.ksoyj.mongodb.net/htmlbricks?retryWrites=true&w=majority",
     "authPath": "/.netlify/functions/auth",
     "jwtSecret": "kgHUHUi;ri8mRTYtr_;r86I67mtWt",
@@ -10,4 +10,11 @@ export const handler = serverless(router({
         issuer: "htmlbricks",
         expiresIn: 60 * 60 * 24
     }
-}))
+})
+const hand = serverless(app);
+export const handler = async (event, context) => {
+    // you can do other things here
+    const result = await hand(event, context);
+    // and here
+    return result;
+};
