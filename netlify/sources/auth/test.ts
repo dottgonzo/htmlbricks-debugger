670d73e83862bb3f0c9d3ecbb34c52154a816cc9
dottg0n20 <dev@dariocaruso.info>
2022-07-09 11:55:44 +0200

    remove netlify functions

--- netlify/sources/auth/test.ts
+++
@@ -1,6 +0,0 @@
-
-module.exports.handler = async (event, context) => {
-    // you can do other things here
-    // and here
-    return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
