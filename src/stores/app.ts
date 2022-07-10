4ab14aac934c065e48a39397857845df39217f41
dottg0n20 <dev@dariocaruso.info>
2022-07-10 02:45:02 +0200

    up

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -6,9 +6,12 @@ export type allComponentsList = {
 	version: string;
 };
 const githubClientId = 'a355c8b84da0daf4b701'
+const gitlabClientId = 'cf6a29d469aeca70ae50611f01ef0188343707270071ee7f6dc3e030a0009d17'
+const gitlabScope = 'email'
 
 const googleClientId = '737686426982-4b7qenmb8jl7rf1j4jp649gai7t3jh00.apps.googleusercontent.com'
 const googleScope = 'https%3A//www.googleapis.com/auth/userinfo.email'
+const githubScope = 'user'
 const productionFontendUrl = 'https://demo.freewebcomponents.com'
 const developmentFontendUrl = 'http://localhost:3000'
 export const authUrl: Readable<string> = readable('https://fn.freewebcomponents.com/.netlify/functions/auth')
@@ -20,16 +23,16 @@ export const oauth2providers: Readable<{ provider: string, uri: string }[]> = re
 	},
 	{
 		provider: 'github',
-		uri: `https://github.com/login/oauth/authorize?scope=user&client_id=${githubClientId}&redirect_uri=${productionFontendUrl}/login?provider=github`
+		uri: `https://github.com/login/oauth/authorize?scope=${githubScope}&client_id=${githubClientId}&redirect_uri=${productionFontendUrl}/login?provider=github`
 	},
 	{
-		provider: 'twitter',
-		uri: ''
+		provider: 'gitlab',
+		uri: `https://gitlab.com/oauth/authorize?scope=${gitlabScope}&response_type=code&state=${new Date().valueOf()}&client_id=${gitlabClientId}&redirect_uri=${productionFontendUrl}/login?provider=gitlab`
 	}
 ])
 export const pageName: Writable<string> = writable('');
 export const pageTitle: Writable<string> = writable('');
-export const componentsVersion: Readable<string> = readable('0.15.14');
+export const componentsVersion: Readable<string> = readable('0.15.16');
 export const debugVersion: Writable<string> = writable(null);
 // export const defaultRootRepo: Readable<string> = readable('@htmlbricks');
 // export const rootRepo: Writable<string> = writable(null);
