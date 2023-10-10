5d11ccc8557dcc0e216582aeff4fa62f69371a6a
dottg0n20 <dev@dariocaruso.info>
2023-10-10 18:13:27 +0200

    update bricks

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -8,7 +8,7 @@ export type allComponentsList = {
 
 export const pageName: Writable<string> = writable('');
 export const pageTitle: Writable<string> = writable('');
-export const componentsVersion: Readable<string> = readable('0.19.28');
+export const componentsVersion: Readable<string> = readable('0.20.0');
 export const debugVersion: Writable<string> = writable(null);
 // export const defaultRootRepo: Readable<string> = readable('@htmlbricks');
 // export const rootRepo: Writable<string> = writable(null);
