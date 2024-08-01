13586579db6f983242e12e5c8c2c9f9539d7d417
Dario Caruso <dcaruso@movia.biz>
2024-08-01 17:50:34 +0200

    chore: update @htmlbricks to version 0.20.14

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -8,7 +8,7 @@ export type allComponentsList = {
 
 export const pageName: Writable<string> = writable('');
 export const pageTitle: Writable<string> = writable('');
-export const componentsVersion: Readable<string> = readable('0.20.13');
+export const componentsVersion: Readable<string> = readable('0.20.14');
 export const debugVersion: Writable<string> = writable(null);
 // export const defaultRootRepo: Readable<string> = readable('@htmlbricks');
 // export const rootRepo: Writable<string> = writable(null);
